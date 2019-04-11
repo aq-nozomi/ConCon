11ebaeeef29d15af23efa88ac8c4433d391bda92
Yotz Ime <cea.tcmboa.4@gmail.com>
2019-04-11 16:33:46 +0900

    initial commit

---
+++ ConCon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConCon
+//
+//  Created by yoshiki imai on 2019/04/11.
+//  Copyright © 2019 nozomi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
